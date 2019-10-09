2878ffb8d9cbc4216e0a4022b6aa0a853ea50627
guy-ar <guy.arbilly@gmail.com>
2019-10-09 11:04:04 +0300

    add integration between the recipe components

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -9,9 +9,10 @@ import { Recipe } from '../recipe.model';
 export class RecipeListComponent implements OnInit {
   recipes: Recipe[] = [
     new Recipe("test", "This is test Recipe", "https://upload.wikimedia.org/wikipedia/commons/3/39/Recipe.jpg"),
-    new Recipe("test", "This is test Recipe", "https://www.nps.gov/subjects/camping/images/recipe_1.jpg?maxwidth=1200&maxheight=1200&autorotate=false"),
-    new Recipe("test", "This is test Recipe", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSBTfd7uJgwgGLtcoel7SfngmrkWLi4fNiG7Yp7GVw_N4v5Ujni")
+    new Recipe("test1", "This is test2 Recipe", "https://www.nps.gov/subjects/camping/images/recipe_1.jpg?maxwidth=1200&maxheight=1200&autorotate=false"),
+    new Recipe("test2", "This is test3 Recipe", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSBTfd7uJgwgGLtcoel7SfngmrkWLi4fNiG7Yp7GVw_N4v5Ujni")
   ];
+  @Output() transferRecipe: EventEmitter<Recipe> = new EventEmitter<Recipe>();
 
   constructor() { 
     // let recipeItem: Recipe = new Recipe("test", "This is test Recipe", "https://upload.wikimedia.org/wikipedia/commons/3/39/Recipe.jpg");
@@ -20,6 +21,10 @@ export class RecipeListComponent implements OnInit {
   }
 
   ngOnInit() {
+  }
+
+  onItem(item: Recipe) {
+    this.transferRecipe.emit(item);
   }
 
 }
